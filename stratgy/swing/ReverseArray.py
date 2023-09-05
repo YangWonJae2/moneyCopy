5946010df8a73f432640ba10239f0e3552254399
DESKTOP-J896IQ6\YangWonJae <yang852039@naver.com>
2023-09-06 00:32:26 +0900

    역배열 - #TODO 추가 : 기본 로직 구상 및 메모

--- stratgy/swing/ReverseArray.py
+++ stratgy/swing/ReverseArray.py
@@ -20,9 +20,6 @@
 # 1. 거래량이 증가
 # 2. 112선을 돌파
 
-# CpSysDib.CpMarketWatchS 특징주 포착
-
-
 import sys
 from PyQt5.QtWidgets import *
 import win32com.client
@@ -30,13 +27,12 @@ import ctypes
 import pandas as pd
 import os
 
-g_objCodeMgr = win32com.client.Dispatch('CpUtil.CpCodeMgr')
+g_objCodeMgr = win32com.client.Dispatch('CpUtil.CpStockCode')
 g_objCpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
 g_objCpTrade = win32com.client.Dispatch('CpTrade.CpTdUtil')
 
-gExcelFile = '8092.xlsx'
 
-
+# InitPlusCheck: 시스템 세팅 체크
 def InitPlusCheck():
     # 프로세스가 관리자 권한으로 실행 여부
     if ctypes.windll.shell32.IsUserAnAdmin():
@@ -50,14 +46,7 @@ def InitPlusCheck():
         print("PLUS가 정상적으로 연결되지 않음. ")
         return False
 
-    '''
-    # 주문 관련 초기화
-    if (g_objCpTrade.TradeInit(0) != 0):
-        print("주문 초기화 실패")
-        return False
-    '''
     return True
-
 
 # CpEvent: 실시간 이벤트 수신 클래스
 class CpEvent:
@@ -65,100 +54,43 @@ class CpEvent:
         self.client = client  # CP 실시간 통신 object
         self.name = name  # 서비스가 다른 이벤트를 구분하기 위한 이름
         self.caller = caller  # callback 을 위해 보관
-        self.diccode = {
-            10: '외국계증권사창구첫매수',
-            11: '외국계증권사창구첫매도',
-            12: '외국인순매수',
-            13: '외국인순매도',
-            21: '전일거래량갱신',
-            22: '최근5일거래량최고갱신',
-            23: '최근5일매물대돌파',
-            24: '최근60일매물대돌파',
-            28: '최근5일첫상한가',
-            29: '최근5일신고가갱신',
-            30: '최근5일신저가갱신',
-            31: '상한가직전',
-            32: '하한가직전',
-            41: '주가 5MA 상향돌파',
-            42: '주가 5MA 하향돌파',
-            43: '거래량 5MA 상향돌파',
-            44: '주가데드크로스(5MA < 20MA)',
-            45: '주가골든크로스(5MA > 20MA)',
-            46: 'MACD 매수-Signal(9) 상향돌파',
-            47: 'MACD 매도-Signal(9) 하향돌파',
-            48: 'CCI 매수-기준선(-100) 상향돌파',
-            49: 'CCI 매도-기준선(100) 하향돌파',
-            50: 'Stochastic(10,5,5)매수- 기준선상향돌파',
-            51: 'Stochastic(10,5,5)매도- 기준선하향돌파',
-            52: 'Stochastic(10,5,5)매수- %K%D 교차',
-            53: 'Stochastic(10,5,5)매도- %K%D 교차',
-            54: 'Sonar 매수-Signal(9) 상향돌파',
-            55: 'Sonar 매도-Signal(9) 하향돌파',
-            56: 'Momentum 매수-기준선(100) 상향돌파',
-            57: 'Momentum 매도-기준선(100) 하향돌파',
-            58: 'RSI(14) 매수-Signal(9) 상향돌파',
-            59: 'RSI(14) 매도-Signal(9) 하향돌파',
-            60: 'Volume Oscillator 매수-Signal(9) 상향돌파',
-            61: 'Volume Oscillator 매도-Signal(9) 하향돌파',
-            62: 'Price roc 매수-Signal(9) 상향돌파',
-            63: 'Price roc 매도-Signal(9) 하향돌파',
-            64: '일목균형표매수-전환선 > 기준선상향교차',
-            65: '일목균형표매도-전환선 < 기준선하향교차',
-            66: '일목균형표매수-주가가선행스팬상향돌파',
-            67: '일목균형표매도-주가가선행스팬하향돌파',
-            68: '삼선전환도-양전환',
-            69: '삼선전환도-음전환',
-            70: '캔들패턴-상승반전형',
-            71: '캔들패턴-하락반전형',
-            81: '단기급락후 5MA 상향돌파',
-            82: '주가이동평균밀집-5%이내',
-            83: '눌림목재상승-20MA 지지'
-        }
 
     def OnReceived(self):
-        print(self.name)
-        # 실시간 처리 - marketwatch : 특이 신호(차트, 외국인 순매수 등)
-        if self.name == 'marketwatch':
-            code = self.client.GetHeaderValue(0)
-            name = g_objCodeMgr.CodeToName(code)
-            cnt = self.client.GetHeaderValue(2)
+        pbData = {}
+        # 실시간 종목검색 감시 처리
+        if self.name == 'cssalert':
+            pbData['전략ID'] = self.client.GetHeaderValue(0)
+            pbData['감시일련번호'] = self.client.GetHeaderValue(1)
+            code = pbData['code'] = self.client.GetHeaderValue(2)
+            pbData['종목명'] = name = g_objCodeMgr.CodeToName(code)
 
-            for i in range(cnt):
-                item = {}
-                newcancel = ''
-                time = self.client.GetDataValue(0, i)
-                h, m = divmod(time, 100)
-                item['시간'] = '%02d:%02d' % (h, m)
-                update = self.client.GetDataValue(1, i)
-                item['코드'] = code
-                item['종목명'] = name
-                cate = self.client.GetDataValue(2, i)
-                if (update == ord('c')):
-                    newcancel = '[취소]'
-                if cate in self.diccode:
-                    item['특이사항'] = newcancel + self.diccode[cate]
-                else:
-                    item['특이사항'] = newcancel + ''
+            inoutflag = self.client.GetHeaderValue(3)
+            if (ord('1') == inoutflag):
+                pbData['INOUT'] = '진입'
+            elif (ord('2') == inoutflag):
+                pbData['INOUT'] = '퇴출'
+            pbData['시각'] = self.client.GetHeaderValue(4)
+            pbData['현재가'] = self.client.GetHeaderValue(5)
+            self.caller.checkRealtimeStg(pbData)
 
-                self.caller.listWatchData.insert(0, item)
-                print(item)
+            # TODO
+            종목리스트 self.monList 에서 [종목코드]의 이전 DATA 조회
+            매집봉 FIND ( 매집봉 거래량 평균 * 3  AND 112선 돌파 )
+            [분기] 매집봉 유무 ?
+            1. 매집봉 있음
+             self.미정.ADD -- 주의대상 LIST IN
+            2. 매집봉 없음
+             self.미정.REMOVE -- 주의대상 LIST OUT
+            
+            [분기] self.미정 Empty ?  -- self.미정의 변경사항이 있을 경우에만 넣는것도 고려
+            1. Empty
+             Clear - 실시간 감시대상 관리
+            2. else
+             감시대상 추가 
 
-        # 실시간 처리 - marketnews : 뉴스 및 공시 정보
-        elif self.name == 'marketnews':
-            item = {}
-            update = self.client.GetHeaderValue(0)
-            cont = ''
-            if update == ord('D'):
-                cont = '[삭제]'
-            code = item['코드'] = self.client.GetHeaderValue(1)
-            time = self.client.GetHeaderValue(2)
-            h, m = divmod(time, 100)
-            item['시간'] = '%02d:%02d' % (h, m)
-            item['종목명'] = name = g_objCodeMgr.CodeToName(code)
-            cate = self.client.GetHeaderValue(4)
-            item['특이사항'] = cont + self.client.GetHeaderValue(5)
-            print(item)
-            self.caller.listWatchData.insert(0, item)
+
+
+
 
 
 class CpPublish:
@@ -185,60 +117,178 @@ class CpPublish:
         self.bIsSB = False
 
 
-# CpPBMarkeWatch:
-class CpPBMarkeWatch(CpPublish):
+# CpPBCssAlert: 종목검색 실시간 PB 클래스
+class CpPBCssAlert(CpPublish):
     def __init__(self):
-        super().__init__('marketwatch', 'CpSysDib.CpMarketWatchS')
+        super().__init__('cssalert', 'CpSysDib.CssAlert')
 
 
-# CpPBMarkeWatch:
-class CpPB8092news(CpPublish):
+# Cp8537 : 종목검색 전략 조회
+class Cp8537:
     def __init__(self):
-        super().__init__('marketnews', 'Dscbo1.CpSvr8092S')
+        self.objpb = CpPBCssAlert()
+        self.bisSB = False
+        self.monList = {}
 
+    def __del__(self):
+        self.Clear()
 
-# CpRpMarketWatch : 특징주 포착 통신
-class CpRpMarketWatch:
-    def __init__(self):
-        self.objStockMst = win32com.client.Dispatch('CpSysDib.CpMarketWatch')
-        self.objpbMarket = CpPBMarkeWatch()
-        self.objpbNews = CpPB8092news()
-        return
+    def Clear(self):
+        self.stopAllStgControl()
+        if self.bisSB:
+            self.objpb.Unsubscribe()
+            self.bisSB = False
 
-    def Request(self, code, caller):
-        self.objpbMarket.Unsubscribe()
-        self.objpbNews.Unsubscribe()
+    def requestList(self, sel):
+        retStgList = {}
+        objRq = win32com.client.Dispatch("CpSysDib.CssStgList")
 
-        self.objStockMst.SetInputValue(0, code)
-        # 1: 종목 뉴스 2: 공시정보 10: 외국계 창구첫매수, 11:첫매도 12 외국인 순매수 13 순매도
-        rqField = '1,2,10,11,12,13'
-        self.objStockMst.SetInputValue(1, rqField)
-        self.objStockMst.SetInputValue(2, 0)  # 시작 시간: 0 처음부터
+        # 예제 전략에서 전략 리스트를 가져옵니다.
+        if (sel == '예제'):
+            objRq.SetInputValue(0, ord('0'))  # '0' : 예제전략, '1': 나의전략
+        else:
+            objRq.SetInputValue(0, ord('1'))  # '0' : 예제전략, '1': 나의전략
+        objRq.BlockRequest()
 
-        ret = self.objStockMst.BlockRequest()
-        if self.objStockMst.GetDibStatus() != 0:
-            print('통신상태', self.objStockMst.GetDibStatus(), self.objStockMst.GetDibMsg1())
-            return False
+        # 통신 및 통신 에러 처리
+        rqStatus = objRq.GetDibStatus()
+        if rqStatus != 0:
+            rqRet = objRq.GetDibMsg1()
+            print("통신상태", rqStatus, rqRet)
+            return (False, retStgList)
 
-        cnt = self.objStockMst.GetHeaderValue(2)  # 수신 개수
+        cnt = objRq.GetHeaderValue(0)  # 0 - (long) 전략 목록 수
+        flag = objRq.GetHeaderValue(1)  # 1 - (char) 요청구분
+        print('종목검색 전략수:', cnt)
+
         for i in range(cnt):
             item = {}
-
-            time = self.objStockMst.GetDataValue(0, i)
-            h, m = divmod(time, 100)
-            item['시간'] = '%02d:%02d' % (h, m)
-            item['코드'] = self.objStockMst.GetDataValue(1, i)
-            item['종목명'] = g_objCodeMgr.CodeToName(item['코드'])
-            cate = self.objStockMst.GetDataValue(3, i)
-            item['특이사항'] = self.objStockMst.GetDataValue(4, i)
+            item['전략명'] = objRq.GetDataValue(0, i)
+            item['ID'] = objRq.GetDataValue(1, i)
+            item['전략등록일시'] = objRq.GetDataValue(2, i)
+            item['작성자필명'] = objRq.GetDataValue(3, i)
+            item['평균종목수'] = objRq.GetDataValue(4, i)
+            item['평균승률'] = objRq.GetDataValue(5, i)
+            item['평균수익'] = objRq.GetDataValue(6, i)
+            retStgList[item['전략명']] = item
             print(item)
-            caller.listWatchData.append(item)
 
-        self.objpbMarket.Subscribe(code, caller)
-        self.objpbNews.Subscribe(code, caller)
+        return (True, retStgList)
 
-        return True
+    def requestStgID(self, id):
+        retStgList = []
+        objRq = None
+        objRq = win32com.client.Dispatch("CpSysDib.CssStgFind")
+        objRq.SetInputValue(0, id)  # 전략 id 요청
+        objRq.BlockRequest()
+        # 통신 및 통신 에러 처리
+        rqStatus = objRq.GetDibStatus()
+        if rqStatus != 0:
+            rqRet = objRq.GetDibMsg1()
+            print("통신상태", rqStatus, rqRet)
+            return (False, retStgList)
 
+        cnt = objRq.GetHeaderValue(0)  # 0 - (long) 검색된 결과 종목 수
+        totcnt = objRq.GetHeaderValue(1)  # 1 - (long) 총 검색 종목 수
+        stime = objRq.GetHeaderValue(2)  # 2 - (string) 검색시간
+        print('검색된 종목수:', cnt, '전체종목수:', totcnt, '검색시간:', stime)
+
+        for i in range(cnt):
+            item = {}
+            item['code'] = objRq.GetDataValue(0, i)
+            item['종목명'] = g_objCodeMgr.CodeToName(item['code'])
+            retStgList.append(item)
+
+        return (True, retStgList)
+
+    def requestMonitorID(self, id):
+        objRq = win32com.client.Dispatch("CpSysDib.CssWatchStgSubscribe")
+        objRq.SetInputValue(0, id)  # 전략 id 요청
+        objRq.BlockRequest()
+
+        # 통신 및 통신 에러 처리
+        rqStatus = objRq.GetDibStatus()
+        if rqStatus != 0:
+            rqRet = objRq.GetDibMsg1()
+            print("통신상태", rqStatus, rqRet)
+            return (False, 0)
+
+        monID = objRq.GetHeaderValue(0)
+        if monID == 0:
+            print('감시 일련번호 구하기 실패')
+            return (False, 0)
+
+        # monID - 전략 감시를 위한 일련번호를 구해온다.
+        # 현재 감시되는 전략이 없다면 감시일련번호로 1을 리턴하고,
+        # 현재 감시되는 전략이 있다면 각 통신 ID에 대응되는 새로운 일련번호를 리턴한다.
+        return (True, monID)
+
+    def requestStgControl(self, id, monID, bStart):
+        objRq = win32com.client.Dispatch("CpSysDib.CssWatchStgControl")
+        objRq.SetInputValue(0, id)  # 전략 id 요청
+        objRq.SetInputValue(1, monID)  # 감시일련번호
+
+        if bStart == True:
+            objRq.SetInputValue(2, ord('1'))  # 감시시작
+            print('전략감시 시작 요청 ', '전략 ID:', id, '감시일련번호', monID)
+        else:
+            objRq.SetInputValue(2, ord('3'))  # 감시취소
+            print('전략감시 취소 요청 ', '전략 ID:', id, '감시일련번호', monID)
+        objRq.BlockRequest()
+
+        # 통신 및 통신 에러 처리
+        rqStatus = objRq.GetDibStatus()
+        if rqStatus != 0:
+            rqRet = objRq.GetDibMsg1()
+            print("통신상태", rqStatus, rqRet)
+            return (False, '')
+
+        status = objRq.GetHeaderValue(0)
+
+        if status == 0:
+            print('전략감시상태: 초기상태')
+        elif status == 1:
+            print('전략감시상태: 감시중')
+        elif status == 2:
+            print('전략감시상태: 감시중단')
+        elif status == 3:
+            print('전략감시상태: 등록취소')
+
+        # event 수신 요청 - 요청 중이 아닌 경우에만 요청
+        if self.bisSB == False:
+            self.objpb.Subscribe('', self)
+            self.bisSB = True
+
+        # 진행 중인 전략들 저장
+        if bStart == True:
+            self.monList[id] = monID
+        else:
+            if id in self.monList:
+                del self.monList[id]
+
+        return (True, status)
+
+    def stopAllStgControl(self):
+        delitem = []
+        for id, monId in self.monList.items():
+            delitem.append((id, monId))
+
+        for item in delitem:
+            self.requestStgControl(item[0], item[1], False)
+
+        print(len(self.monList))
+
+    def checkRealtimeStg(self, pbData):
+        # 감시중인 전략인 경우만 체크
+        id = pbData['전략ID']
+        monid = pbData['감시일련번호']
+        if not (id in self.monList):
+            return
+
+        if (monid != self.monList[id]):
+            return
+
+        print(pbData)
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -248,56 +298,85 @@ class MyWindow(QMainWindow):
         if InitPlusCheck() == False:
             exit()
 
-        self.listWatchData = []
-        self.objMarketWatch = CpRpMarketWatch()
+        self.setWindowTitle("종목검색 예제")
+        self.setGeometry(300, 300, 500, 180)
 
-        self.setWindowTitle("특징주 포착(#8092 Market-Watch")
-        self.setGeometry(300, 300, 300, 180)
+        self.obj8537 = Cp8537() # Cp8537 : 종목검색 전략 조회
+        self.dataStg = []
 
         nH = 20
-
-        btnPrint = QPushButton('Print', self)
-        btnPrint.move(20, nH)
-        btnPrint.clicked.connect(self.btnPrint_clicked)
-        nH += 50
-
-        btnExcel = QPushButton('Excel 내보내기', self)
-        btnExcel.move(20, nH)
-        btnExcel.clicked.connect(self.btnExcel_clicked)
+        self.comboStg = QComboBox(self)
+        self.comboStg.move(20, nH)
+        self.comboStg.currentIndexChanged.connect(self.comboChanged)
+        self.comboStg.resize(400, 30)
         nH += 50
 
         btnExit = QPushButton('종료', self)
         btnExit.move(20, nH)
         btnExit.clicked.connect(self.btnExit_clicked)
         nH += 50
+        self.setGeometry(300, 300, 500, nH)
 
-        self.objMarketWatch.Request('*', self)
+        self.listAllStrategy()
 
-    def btnPrint_clicked(self):
-        for item in self.listWatchData:
-            print(item)
+    def __del__(self):
+        self.obj8537.Clear()
+
+    # 전략리스트 조회
+    def listAllStrategy(self):
+        self.comboStg.addItem('전략선택없음')
+        self.data8537 = {}
+        #ret, self.data8537 = self.obj8537.requestList('예제')
+        ret, self.data8537 = self.obj8537.requestList('')
+
+        for k, v in self.data8537.items():
+            self.comboStg.addItem(k)
         return
 
-    def btnExcel_clicked(self):
-
-        if (len(self.listWatchData) == 0):
-            print('데이터 없음')
+    def comboChanged(self):
+        stgName = self.comboStg.currentText()
+        print(stgName)
+        if (stgName == '전략선택없음'):
             return
 
-        df = pd.DataFrame(columns=['시간', '코드', '종목명', '특이사항'])
+        # 1: 기존 감시 중단 (중요)
+        # 종목검색 실시간 감시 개수 제한이 있어, 불필요한 감시는 중단이 필요
+        self.obj8537.Clear()
 
-        for item in self.listWatchData:
-            df.loc[len(df)] = item
+        # 2 - 종목검색 조회: CpSysDib.CssStgFind
+        item = self.data8537[stgName]
+        id = item['ID']
+        name = item['전략명']
 
-        writer = pd.ExcelWriter(gExcelFile, engine='xlsxwriter')
-        # Convert the dataframe to an XlsxWriter Excel object.
-        df.to_excel(writer, sheet_name='Sheet1')
-        # Close the Pandas Excel writer and output the Excel file.
-        writer.save()
-        os.startfile(gExcelFile)
+        ret, self.dataStg = self.obj8537.requestStgID(id)
+        if ret == False:
+            return
+
+        for item in self.dataStg:
+            print(item)
+        print('검색전략:', id, '전략명:', name, '검색종목수:', len(self.dataStg))
+
+        if (len(self.dataStg) >= 200):
+            print('검색종목이 200 을 초과할 경우 실시간 감시 불가 ')
+            return
+
+        #####################################################
+        # 실시간 요청
+        # 3 - 전략의 감시 일련번호 요청 : CssWatchStgSubscribe
+        ret, monid = self.obj8537.requestMonitorID(id)
+        if (False == ret):
+            return
+        print('감시일련번호', monid)
+
+        # 4 - 전략 감시 시작 요청 - CpSysDib.CssWatchStgControl
+        ret, status = self.obj8537.requestStgControl(id, monid, True)
+        if (False == ret):
+            return
+
         return
 
     def btnExit_clicked(self):
+        self.obj8537.Clear()
         exit()
         return
 
